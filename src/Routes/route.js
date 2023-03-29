46032defbffa5eb1e506f602ceaece7fa7822937
AbhijitRadke <radkeabhi27@gmail.com>
2023-03-29 22:33:34 +0530

    v5

--- src/Routes/route.js
+++ src/Routes/route.js
@@ -3,6 +3,24 @@ const router = Router()
 const userController = require("../Controllers/userControllers")
 const middlewares = require("../Middleware/auth")
 
+
+/**
+ * @swagger
+ * /route:
+ *   get:
+ *     summary: Returns the list of all the books
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: The list of the Users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/Models/userModel'
+ */
+
 router.post("/users", userController.userCreate)
 
 router.post("/login", userController.userLogin)
@@ -14,6 +32,18 @@ router.get("/users/:Id", middlewares.authentication, middlewares.authorization, 
 router.put("/users/:Id", middlewares.authentication, middlewares.authorization, userController.updateUser)
 
 router.delete("/users/:Id", middlewares.authentication, middlewares.authorization, userController.deleteUserById)
+
+
+// router.all("/**", (req, res) => {
+//     return res.status(400).send({ status: false, message: "your URL is wrong plese check endpoint" })
+// })
+
+
+
+
+
+
+
 
 
 module.exports = router
--- src/index.js
+++ src/index.js
@@ -3,6 +3,40 @@ const app = express()
 const mongoose = require('mongoose')
 const route = require('./routes/route')
 
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Users API Assignment",
+            version: "1.0.0",
+            description: "A simple API to perform CRUD oprations on user",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+    },
+    apis: ["./route/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+
+
+
+
+
+
+
+
+
+
 app.use(express.json())
 mongoose.set('strictQuery', true)
 
